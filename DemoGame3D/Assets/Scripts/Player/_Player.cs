e443ffa42145af0e04b5777a67ea2305f149a9bc
nghiadotuan <dotuannghia77@gmail.com>
2022-07-24 09:15:25 +0700

    commit

---
+++ DemoGame3D/Assets/Scripts/Player/_Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Animator _animator;
+    
+    
+}
